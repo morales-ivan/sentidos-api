221db5660547e41c69a3a4ad72519bb5eddc087e
nulmete <nicoulmete1@gmail.com>
2021-12-22 09:55:49 -0300

    add acl to s3 presigned url

--- src/main/java/com/sirius/sentidosapi/controllers/S3Controller.java
+++ src/main/java/com/sirius/sentidosapi/controllers/S3Controller.java
@@ -1,13 +1,16 @@
 package com.sirius.sentidosapi.controllers;
 
 import com.sirius.sentidosapi.services.S3Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import java.util.Map;
 
 @RestController
@@ -19,8 +22,20 @@ public class S3Controller {
         this.s3Service = s3Service;
     }
 
+    // @PostMapping
+    // public String generatePresignedUrl(@RequestBody @Valid String fileName) {
+    //   return s3Service.generateUrl(fileName);
+    // }
+
+    // @GetMapping
+    // public ResponseEntity<Object> getFile(HttpServletRequest request, @RequestBody(required = false) Map<String, String> params) {
+    //     final String path = request.getServletPath();
+    //     if (params.containsKey(FILE_NAME))
+    //         return new ResponseEntity<>(s3Service.getObject(params.get(FILE_NAME)), HttpStatus.OK);
+    // }
+
     @PostMapping
-    public String generatePresignedUrl(@RequestBody @Valid String fileName) {
-      return s3Service.generateUrl(fileName);
+    public ResponseEntity<Object> saveFile(@RequestParam("extension") String extension) {
+        return new ResponseEntity<>(s3Service.putObject(extension), HttpStatus.OK);
     }
 }
--- src/main/java/com/sirius/sentidosapi/services/S3Service.java
+++ src/main/java/com/sirius/sentidosapi/services/S3Service.java
@@ -3,16 +3,17 @@ package com.sirius.sentidosapi.services;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.Headers;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Async;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @Slf4j
@@ -24,7 +25,7 @@ public class S3Service {
     @Value("${aws.bucket}")
     private String s3BucketName;
 
-    public String generateUrl(String fileName) {
+    public String generateUrl(String fileName, HttpMethod httpMethod) {
          try {
             // Set the presigned URL to expire after one hour.
             final java.util.Date expiration = new java.util.Date();
@@ -32,12 +33,11 @@ public class S3Service {
             expTimeMillis += 1000 * 60 * 60;
             expiration.setTime(expTimeMillis);
 
-            // Generate the presigned URL.
-            log.info("Generating pre-signed URL.");
             GeneratePresignedUrlRequest generatePresignedUrlRequest =
                     new GeneratePresignedUrlRequest(this.s3BucketName, fileName)
-                            .withMethod(HttpMethod.PUT)
+                            .withMethod(httpMethod)
                             .withExpiration(expiration);
+            generatePresignedUrlRequest.addRequestParameter(Headers.S3_CANNED_ACL, CannedAccessControlList.PublicRead.toString());
             return this.amazonS3.generatePresignedUrl(generatePresignedUrlRequest).toString();
         } catch (AmazonServiceException e) {
             // The call was transmitted successfully, but Amazon S3 couldn't process
@@ -45,5 +45,26 @@ public class S3Service {
             e.printStackTrace();
             return null;
         }
+    }
+
+    // @Async
+    // public String getObject(String fileName) {
+    //     if (!amazonS3.doesObjectExist(this.s3BucketName, fileName))
+    //         return "File does not exist";
+    //     log.info("Generating pre-signed URL for file name {}", fileName);
+    //     return generateUrl(fileName, HttpMethod.GET);
+    // }
+
+    @Async
+    public Map<String, ?> putObject(String extension) {
+        String fileName = UUID.randomUUID().toString() + extension;
+        log.info("Generating pre-signed URL to upload an object.");
+        String generatedUrl = generateUrl(fileName, HttpMethod.PUT);
+        String objectUrl = "https://" + this.s3BucketName + ".s3.amazonaws.com/" + fileName;
+        Map<String, String> map = new HashMap<String, String>();
+
+        map.put("uploadUrl", generatedUrl);
+        map.put("objectUrl", objectUrl);
+        return map;
     }
 }
